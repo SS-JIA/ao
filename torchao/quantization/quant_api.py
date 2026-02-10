ecc59d06bf2d2ce8aefb89ee6d22c94b802803b8
Daniel Vega-Myhre <danvm@meta.com>
2026-02-10 17:56:10 +0000

    [moe training] refactor configs and support converting linears + grouped gemms in a single quantize_() call

--- torchao/quantization/quant_api.py
+++ torchao/quantization/quant_api.py
@@ -1939,10 +1939,17 @@ ModuleFqnToConfig = FqnToConfig
 # for now, we need to keep track of what configs support custom param quantization.
 # Once we've updated all the transform functions to take in a custom_param kwarg, we can delete this object and the subsequent check
 # TODO see https://github.com/pytorch/ao/issues/3252 for more details
+from torchao.prototype.moe_training.config import (
+    FP8GroupedMMConfig,
+    MXFP8GroupedMMConfig,
+)
+
 CUSTOM_PARAM_QUANTIZATION_SUPPORTED_CONFIGS = {
     Float8DynamicActivationFloat8WeightConfig,
     Float8WeightOnlyConfig,
     Int8WeightOnlyConfig,
+    MXFP8GroupedMMConfig,
+    FP8GroupedMMConfig,
 }
 
 
